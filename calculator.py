1daa2f2013e1197b5dcf094bce20163b29dc5ef8
PythonicParadox <70861235+PythonicParadox@users.noreply.github.com>
2021-05-01 11:51:49 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,27 +1,27 @@
 num1 = int(input("Enter a number:\n"))
 op = input("Select an operator.\n")
 num2 = int(input("Enter a number:\n"))
-
-if op == "+":
-    add = num1 + num2
-    print("Answer:", add)
-elif op == "-":
-    sub = num1 - num2
-    print("Answer:", sub)
-elif op == "*":
-    mul = num1 * num2
-    print("Answer:", mul)
-elif op == "/":
-    div = num1 / num2
-    print("Answer:", div)
-elif op == "**":
-    pov = num1 ** num2
-    print("Answer:", pov)
-i = input("Do you want to continue?")
-if i == "Yes":
-    continue
-elif i == "yes":
-    continue
-else:
-    break
+while True:
+    if op == "+":
+        add = num1 + num2
+        print("Answer:", add)
+    elif op == "-":
+        sub = num1 - num2
+        print("Answer:", sub)
+    elif op == "*":
+        mul = num1 * num2
+        print("Answer:", mul)
+    elif op == "/":
+        div = num1 / num2
+        print("Answer:", div)
+    elif op == "**":
+        pov = num1 ** num2
+        print("Answer:", pov)
+    i = input("Do you want to continue?")
+    if i == "Yes":
+        continue
+    elif i == "yes":
+        continue
+    else:
+        break
     
